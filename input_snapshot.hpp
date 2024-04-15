f88f8c876d575d5b8cec4414ce54845b85488af1
cuppajoeman <ccn@cuppajoeman.com>
2024-04-15 00:06:00 -0400

    add mouse positions to input snapshot

--- input_snapshot.hpp
+++ input_snapshot.hpp
@@ -7,12 +7,13 @@
 
 class InputSnapshot {
 public:
-    bool left_pressed = false;
-    bool right_pressed = false;
-    bool forward_pressed = false;
-    bool backward_pressed = false;
-    bool jump_pressed = false;
-    glm::vec3 input_snapshot_to_input_direction(Camera camera);
+  bool left_pressed = false;
+  bool right_pressed = false;
+  bool forward_pressed = false;
+  bool backward_pressed = false;
+  bool jump_pressed = false;
+  double mouse_position_x = 0.0, mouse_position_y = 0.0;
+  glm::vec3 input_snapshot_to_input_direction(Camera camera);
 };
 
-#endif //INPUT_SNAPSHOT_HPP
+#endif // INPUT_SNAPSHOT_HPP
