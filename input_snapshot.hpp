622e99eb799032181ae1e9304312da01733fcfc4
cuppajoeman <ccn@cuppajoeman.com>
2024-04-17 20:38:10 -0400

    clangd refactor

--- input_snapshot.hpp
+++ input_snapshot.hpp
@@ -2,18 +2,17 @@
 #define INPUT_SNAPSHOT_HPP
 
 #include <glm/glm.hpp>
-
 #include "../camera/camera.hpp"
 
 class InputSnapshot {
-public:
-  bool left_pressed = false;
-  bool right_pressed = false;
-  bool forward_pressed = false;
-  bool backward_pressed = false;
-  bool jump_pressed = false;
-  double mouse_position_x = 0.0, mouse_position_y = 0.0;
-  glm::vec3 input_snapshot_to_input_direction(Camera camera);
+  public:
+    bool left_pressed = false;
+    bool right_pressed = false;
+    bool forward_pressed = false;
+    bool backward_pressed = false;
+    bool jump_pressed = false;
+    double mouse_position_x = 0.0, mouse_position_y = 0.0;
+    glm::vec3 input_snapshot_to_input_direction(Camera camera);
 };
 
 #endif // INPUT_SNAPSHOT_HPP
